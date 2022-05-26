b60935e186b6682b4a803df20977e0b9dad68572
aluno20230 <aluno20230@ipt.pt>
2022-05-26 21:27:00 +0100

    Primeiras alterações em React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-//App.js
+// App.js
+// *******************************************************************
+
+// importar um conteúdo e colocá-lo numa variável
 import logo from './logo.svg';
+// importar ficheiro CSS
 import './App.css';
 
+
+/* os COMPONENTES podem ser:
+     - classes
+     - funções
+*/
 function App() {
+  // todo o 'componente' tem sempre que devolver alguma coisa...
+  // e devolver sempre um único objeto
   return (
-    <div className="App">
+    <div className="App"> {/* qd quero referir 'class' do CSS,
+                              tenho de usar o termo 'className'.
+                              Depois o JSX, em runtime converte para 'class' */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* no REACT, acede-se ao valor das variáveis através de {} */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +33,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Olá Mundo (2ª vez)</h1>
+      <footer className='App-footer'>
+        &copy; app@DW
+      </footer>
     </div>
   );
 }
 
+// só se consegue aceder ao conteúdo do componente, se ele for 'exportado'
 export default App;
